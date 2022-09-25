36d7610f93bafe4079a05606ab77cfa3622bfb39
RifkiFiransah <muhammadrifkifiransah@gmail.com>
2022-09-25 08:39:51 +0700

    navbar path image

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -23,7 +23,7 @@
     <nav class="navbar sticky-top navbar-expand-lg navbar-light ">
         <div class="container ">
             <a class="navbar-brand" href="#">
-                <img src="/public/image/logo_sipaku.png" alt="" width="30" height="24">Logo
+                <img src="/image/logo_sipaku.png" alt="" width="80">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
