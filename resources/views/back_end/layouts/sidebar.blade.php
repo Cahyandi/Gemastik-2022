3898a64eeb7c5ff6f213ed6bf61499b74f3703ae
RifkiFiransah <muhammadrifkifiransah@gmail.com>
2022-10-13 08:22:58 +0700

    logout di sidebar

--- resources/views/back_end/layouts/sidebar.blade.php
+++ resources/views/back_end/layouts/sidebar.blade.php
@@ -32,25 +32,41 @@
             @if (auth('dinas')->user()->role == 'admin')
             <li class="nav-item">
               <a class="nav-link {{ Request::is('data-petugas*') ? 'active' : '' }}" href="/data-petugas">
-                <span data-feather="file-text" class="align-text-bottom"></span>
+                <i class="fa-solid fa-user-plus me-1"></i>
                 Data Petugas
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link {{ Request::is('data-user*') ? 'active' : '' }}" href="/data-user">
-                <span data-feather="file-text" class="align-text-bottom"></span>
+                <i class="fa-solid fa-users me-1"></i>
                 Data User
               </a>
             </li>
             @else
             <li class="nav-item">
               <a class="nav-link {{ Request::is('data-ticket*') ? 'active' : '' }}" href="/data-ticket/{{ auth('dinas')->user()->username }}">
-                <span data-feather="file-text" class="align-text-bottom"></span>
+                <i class="fa-solid fa-ticket me-1"></i>
                 Data Ticket Masuk
               </a>
             </li>
             @endif
-          </ul>  
+          </ul> 
+          
+          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+            <span>Laporan</span>
+            <a class="link-secondary" href="#" aria-label="Add a new report">
+              <span data-feather="plus-circle" class="align-text-bottom"></span>
+            </a>
+          </h6>
+          <ul class="nav flex-column mb-2">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                {{-- <span data-feather="file-text" class="align-text-bottom"></span> --}}
+                <i class="fa-sharp fa-solid fa-chart-simple me-2"></i>
+                Laporan Pemasukan
+              </a>
+            </li>
+          </ul>
         @else
           <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
             <span>Transaki</span>
@@ -61,32 +77,24 @@
           <ul class="nav flex-column mb-2">
             <li class="nav-item">
               <a class="nav-link" href="/ticket">
-                <span data-feather="file-text" class="align-text-bottom"></span>
+                <i class="fa-solid fa-check-to-slot me-2"></i>
                 Pemesanan
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/riwayat-ticket/{{ auth()->user()->username }}">
-                <span data-feather="file-text" class="align-text-bottom"></span>
+                {{-- <span data-feather="file-text" class="align-text-bottom"></span> --}}
+                <i class="fa-solid fa-clock-rotate-left me-2"></i>
                 Riwayat Ticket
               </a>
             </li>
           </ul>
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-            <span>Laporan</span>
-            <a class="link-secondary" href="#" aria-label="Add a new report">
-              <span data-feather="plus-circle" class="align-text-bottom"></span>
-            </a>
-          </h6>
-          <ul class="nav flex-column mb-2">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file-text" class="align-text-bottom"></span>
-                Laporan Pemasukan
-              </a>
-            </li>
-          </ul>
         @endif
-
+        
+        <h6 class="sidebar-heading d-flex justify-content-center align-items-center px-3 mb-1 text-muted text-uppercase" style="margin-top: 80px;">
+          <a class="nav-link px-3 d-flex flex-column  justify-content-center align-items-center" href="/logout">Sign out
+            <span data-feather="log-out" class="align-text-bottom"></span>
+          </a>
+        </h6>
       </div>
     </nav>
